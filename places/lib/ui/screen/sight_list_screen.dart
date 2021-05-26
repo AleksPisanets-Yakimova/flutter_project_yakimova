b462392868e06acccc3d6f0adf73b1cf29a17d3c
AleksPisanets-Yakimova <aleks.pisanets@mail.ru>
2021-05-26 21:48:12 +1000

    pr для задания с текстом

--- places/lib/ui/screen/sight_list_screen.dart
+++ places/lib/ui/screen/sight_list_screen.dart
@@ -6,7 +6,31 @@ class SightListScreen extends StatefulWidget {
 }
 
 class SightListScreenState extends State<SightListScreen> {
-  final t = 'Hello!';
+  final t = 'Список интересных мест';
+  final r = 'Список интересных мест';
+
+  // ignore: prefer_void_to_null
+  Null get color => null;
   @override
-  Widget build(BuildContext context) => Scaffold(body: Center(child: Text(t)));
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(t),
+          elevation: 0,
+        ),
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(color: color),
+            width: double.infinity,
+            height: 100,
+            child: Text(
+              r,
+              maxLines: 2,
+              style: const TextStyle(
+                fontSize: 50,
+                fontFamily: 'Roboto',
+              ),
+            ),
+          ),
+        ),
+      );
 }
